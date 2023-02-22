f794c393818e6ffa1aabe9f8e4c1f5e3b9060977
Felix Arkle <72893613+Bumsparkle@users.noreply.github.com>
2023-02-22 22:37:16 +0000

    pray

--- public/index.js
+++ public/index.js
@@ -4,8 +4,9 @@ const axios = require('axios');
 //const ethers = require('ethers');
 const { ethers } = require('ethers');
 const { createAlchemyWeb3 } = require('@alch/alchemy-web3');
+require("dotenv").config()
 
-const OPEN_AI_KEY = "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"
+const OPEN_AI_KEY = process.env.OPENAI_KEY
 
 const app = express();
 app.use(bodyParser.json());
@@ -25,6 +26,57 @@ const contractAddress = '0xD06a9Feb63081Ec37F1fA188d9B3D9737dC63716';
 const contract = new ethers.Contract(contractAddress, contractABI, alchemyWeb3);
 
 
+// Define a function to create an NFT
+const createNFT = async (nftData) => {
+  // Prompt the user to connect their wallet
+  if (typeof window.ethereum === 'undefined') {
+    throw new Error('Please install MetaMask or another Web3 wallet to use this dApp!');
+  }
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  await window.ethereum.enable();
+  const signer = provider.getSigner();
+
+  // Create the NFT on the blockchain
+  const createResult = await contract.createNFT(nftData.tokenURI);
+
+  // Get the ID of the created NFT
+  const nftId = createResult.events[0].args.tokenId.toString();
+
+  // Set the metadata for the NFT
+  const metadata = {
+    name: nftData.name,
+    description: nftData.description,
+    image: nftData.image,
+  };
+
+  const setMetadataResult = await contract.setMetadata(nftId, metadata);
+
+  // Transfer ownership of the NFT to the user's wallet
+  const transferResult = await contract.transferFrom(contractAddress, nftData.owner, nftId);
+
+  // Return the ID of the created NFT
+  return nftId;
+};
+
+$(document).ready(function() {
+  $('#create-nft-form').submit(async function(event) {
+    event.preventDefault();
+
+    const nftData = {
+      tokenURI: 'https://dalle.ai/api/generate',
+      name: 'My NFT',
+      description: 'This is my first NFT',
+      image: 'https://dalle.ai/api/generate'
+    };
+
+    try {
+      const nftId = await createNFT(nftData);
+      alert(`NFT created with ID: ${nftId}`);
+    } catch (error) {
+      alert(`Failed to create NFT: ${error.message}`);
+    }
+  });
+});
 
 
 
@@ -44,7 +96,7 @@ app.post('/generate-nft', async (req, res) => {
       response_format: 'url',
     }, {
       headers: {
-        Authorization: `Bearer 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`,
+        Authorization: `Bearer ${OPEN_AI_KEY}`,
       },
     });
 
